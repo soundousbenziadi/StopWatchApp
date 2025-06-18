c3de78af9c31bf30c68af3141e61f8d55a4c432e
soundous <bsoundous112@gmail.com>
2025-06-18 20:33:55 +0100

    Routing with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 function App() {
 
 
   return (
-    <>
+    <main className='main-container'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
 
-    </>
+    </main>
   )
 }
 
